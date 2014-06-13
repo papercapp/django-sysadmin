d525614c2fb3586e0ffc2a7897dc136d0ff9c41e
Hannes Tismer <hannes.tismer@paperc.de>
2014-06-13 11:39:32 +0200

    add configurable default shell; add gitignore

--- django_sysadmin/proftpd/models.py
+++ django_sysadmin/proftpd/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 FTP_DEFAULT_UID = getattr(settings, "FTP_DEFAULT_UID", 1000)
 FTP_DEFAULT_GID = getattr(settings, "FTP_DEFAULT_GID", 1000)
 FTP_DEFAULT_HOMEDIR_BASE = getattr(settings, "FTP_DEFAULT_HOMEDIR_BASE", "/home")
+FTP_DEFAULT_SHELL = getattr(settings, "FTP_DEFAULT_SHELL", "/bin/false")
 
 class FTPUser(models.Model):
     """
@@ -22,7 +23,7 @@ class FTPUser(models.Model):
     gid = models.IntegerField(default=FTP_DEFAULT_GID)
 
     homedir = models.CharField(max_length=255, blank=True, help_text=u"Default: %s" % os.path.join(FTP_DEFAULT_HOMEDIR_BASE, "username"))
-    shell = models.CharField(max_length=255, default="/bin/false")
+    shell = models.CharField(max_length=255, default=FTP_DEFAULT_SHELL)
 
     #optional comment
     comment = models.CharField(max_length=255, null=True, blank=True)
