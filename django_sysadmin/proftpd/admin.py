d5d6eaafc6b44aef777939c4d5b477a794de61b7
Hannes Tismer <hannes.tismer@paperc.com>
2015-11-12 16:47:40 +0100

    django 1.8 compatibility

--- django_sysadmin/proftpd/admin.py
+++ django_sysadmin/proftpd/admin.py
@@ -1,37 +1,39 @@
 from django_sysadmin.proftpd import models
-
 from django.contrib import admin
 from django.contrib.auth.hashers import make_password
 from django import forms
 from django.conf import settings
-
 from models import FTPUser
 
 FTP_PASSWORD_HASHER = getattr(settings, "FTP_PASSWORD_HASHER", "sha1")
 FTP_PASSWORD_SALT_FILE = getattr(settings, "FTP_PASSWORD_SALT_FILE", None)
 
-assert(bool(FTP_PASSWORD_SALT_FILE) == True)
+assert (bool(FTP_PASSWORD_SALT_FILE) == True)
 
 
-
-### actions
+#
+# actions
 
 def setactive(modeladmin, request, queryset):
     queryset.update(active=True)
     setactive.short_description = "Set selected entries as active"
 
+
 def setinactive(modeladmin, request, queryset):
     queryset.update(active=False)
     setinactive.short_description = "Set selected entries as NOT active"
 
-## admin objects
+
+#
+# admin objects
 
 class FTPUserForm(forms.ModelForm):
     class Meta:
         model = FTPUser
         widgets = {
-            'password': forms.PasswordInput(render_value = True),
+            'password': forms.PasswordInput(render_value=True),
         }
+        fields = '__all__'
 
     def clean_password(self):
         passwd = self.cleaned_data.get("password")
@@ -46,10 +48,9 @@ class FTPUserForm(forms.ModelForm):
 class FTPUserAdmin(admin.ModelAdmin):
     form = FTPUserForm
     list_display = ('username', 'homedir', 'uid', 'gid', 'active', 'created', 'comment')
-    list_filter = ('active', 'created' )
+    list_filter = ('active', 'created')
     search_fields = ['username', 'comment']
     actions = [setactive, setinactive]
-
 
 
 admin.site.register(models.FTPUser, FTPUserAdmin)
